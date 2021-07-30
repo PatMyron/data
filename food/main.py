f3e091c97d389c5499b006c6436c1585aca9bfc3
Pat Myron <pmyron@nd.edu>
2021-07-29 22:54:38 -0400

    graphing fruit and vegetable cost and longevity
    https://www.pma.com/content/articles/top-20-fruits-and-vegetables-sold-in-the-us
    https://www.ers.usda.gov/webdocs/publications/42549/15151_aib790d_1_.pdf
    https://www.eatbydate.com/fruits/
    https://www.eatbydate.com/vegetables/
    https://www.stilltasty.com/
    https://unicode.org/emoji/emoji-requests.html

---
+++ food/main.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+
+fruit = {
+    'banana': [.45, 7],
+    'apple': [.9, 50],
+    'strawberry': [1.5, 7],
+    'grape': [1.3, 7],
+    'orange': [.75, 50],
+    'watermelon': [.3, 7],
+    'blueberry': [1.5, 7],
+    'lemon': [2, 50],
+    'peach': [1, 7],
+    'avocado': [1, 7],
+    'pineapple': [1.4, 7],
+    'cherry': [1.7, 7],
+    'cantaloupe': [.6, 11],
+    'pear': [.9, 10],
+    'lime': [1.5, 50],
+    'raspberry/blackberry': [4, 3],
+    'clementine': [1.1, 11],
+    'plum': [1.2, 4],
+    'nectarine': [1, 4],
+    'mango': [.8, 12],
+}
+for i in fruit:
+    plt.plot(fruit[i][1], fruit[i][0], 'oC0')
+    plt.annotate(i, (fruit[i][1], fruit[i][0]))
+veg = {
+    'potato': [.3, 100],
+    'tomato': [1.2, 14],
+    'onion': [.5, 150],
+    'carrot': [.5, 30],
+    'lettuce': [1, 7],
+    'bell pepper': [1.1, 14],
+    'broccoli': [.9, 10],
+    'cucumber': [.75, 7],
+    'celery': [.8, 25],
+    'salad/mushroom': [3, 7],
+    'corn': [.9, 7],
+    'garlic': [2, 150],
+    'spinach': [1.4, 7],
+    'green bean': [1.1, 7],
+    'sweet potato': [.6, 90],
+    'green onion': [1.2, 7],
+    'cauliflower': [1.1, 14],
+    'cabbage': [.3, 30],
+    'asparagus': [1.7, 7],
+}
+for i in veg:
+    plt.plot(veg[i][1], veg[i][0], 'oC2')
+    plt.annotate(i, (veg[i][1], veg[i][0]))
+plt.xlabel('Days to expire')
+plt.ylabel('USD per pound')
+plt.show()
